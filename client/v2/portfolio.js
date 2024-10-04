ebdcb99432d468ab19619e2d144ba6b76e8643b8
brilleaud-claire <claire.brilleaud@gmail.com>
2024-10-04 14:42:22 +0200

    feat(dislpay): show vinted deals

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -25,6 +25,10 @@ This endpoint accepts the following optional query string parameters:
 let currentDeals = [];
 let currentPagination = {};
 
+// current deals on the page
+let currentDealsVinted = [];
+let currentPaginationVinted = {};
+
 // instantiate the selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
@@ -32,6 +36,7 @@ const selectFilter = document.querySelector('#filters');
 const selectSort = document.querySelector('#sort-select');
 const selectLegoSetIds = document.querySelector('#lego-set-id-select');
 const sectionDeals= document.querySelector('#deals');
+const sectionDealsVinted= document.querySelector('#VintedDeals');
 const spanNbDeals = document.querySelector('#nbDeals');
 
 /**
@@ -42,6 +47,16 @@ const spanNbDeals = document.querySelector('#nbDeals');
 const setCurrentDeals = ({result, meta}) => {
   currentDeals = result;
   currentPagination = meta;
+};
+
+/**
+ * Set global value
+ * @param {Array} result - deals to display
+ * @param {Object} meta - pagination meta info
+ */
+const setCurrentDealsVinted = ({result, meta}) => {
+  currentDealsVinted = result;
+  currentPaginationVinted = meta;
 };
 
 /**
@@ -66,6 +81,30 @@ const fetchDeals = async (page = 1, size = 6) => {
   } catch (error) {
     console.error(error);
     return {currentDeals, currentPagination};
+  }
+};
+
+/**
+ * Fetch deals Vinted from api 
+ * @param  {String}  [id="21345"] - current ID to fetch
+ * @return {Object}
+ */
+const fetchVintedDeals = async (id = "21345") => {
+  try {
+    const response = await fetch(
+      `https://lego-api-blue.vercel.app/sales?id=${id}`
+    );
+    const body = await response.json();
+
+    if (body.success !== true) {
+      console.error(body);
+      return {currentDealsVinted, currentPaginationVinted};
+    }
+
+    return body.data;
+  } catch (error) {
+    console.error(error);
+    return {currentDealsVinted, currentPaginationVinted};
   }
 };
 
@@ -94,6 +133,35 @@ const renderDeals = deals => {
   sectionDeals.innerHTML = '<h2>Deals</h2>';
   sectionDeals.appendChild(fragment);
 };
+
+/**
+ * Render list of deals Vinted
+ * @param  {Array} deals
+ */
+const renderDealsVinted = deals => {
+  const fragment = document.createDocumentFragment();
+  const div = document.createElement('div');
+  const template = deals
+    .map(deal => {
+      return `
+      <div class="VintedDeals" id=${deal.uuid}>
+        
+        <a href="${deal.link}">${deal.title}</a>
+        <span>${deal.price}</span>
+      </div>
+    `;
+    })
+    .join('');
+
+  div.innerHTML = template;
+  fragment.appendChild(div);
+  if (sectionDealsVinted) {
+    sectionDealsVinted.innerHTML = '<h3>VintedDeals</h3>';
+    sectionDealsVinted.appendChild(fragment);
+  } else {
+    console.error("Element sectionDealsVinted not found");
+  }
+}
 
 /**
  * Render page selector
@@ -141,6 +209,13 @@ const render = (deals, pagination) => {
   renderLegoSetIds(deals)
 };
 
+const renderVinted = (deals, pagination) => {
+  renderDealsVinted(deals);
+  renderPagination(pagination);
+  renderIndicators(pagination);
+  //renderLegoSetIds(deals)
+};
+
 /**
  * Declaration of all Listeners
  */
@@ -163,6 +238,16 @@ selectPage.addEventListener('change', async (event) => {
 
   setCurrentDeals(deals);
   render(currentDeals, currentPagination);
+});
+
+/**
+ * Select the id vinted to display
+ */
+selectLegoSetIds.addEventListener('change', async (event) => {
+  const deals = await fetchVintedDeals(event.target.value);
+  console.log(event.target.value);
+  setCurrentDealsVinted(deals);
+  renderVinted(currentDealsVinted, currentPagination);
 });
 
 /**
@@ -227,13 +312,9 @@ document.addEventListener('DOMContentLoaded', async () => {
   render(currentDeals, currentPagination);
 });
 
+console.log(currentDeals);
+
 /**
-
-Feature 5 - Sort by date
-As a user
-I want to sort by date
-So that I can easily identify recent and old deals
-
 Feature 7 - Display Vinted sales
 As a user for a given set id
 I want to display vinted sales
